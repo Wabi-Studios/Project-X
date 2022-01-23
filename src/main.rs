1b67df30f43589c696f62d1fd8a5d34632054d67
furby™ <wabi.film@gmail.com>
2022-01-23 10:25:22 -0700

    Prep runtime.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,39 @@
-fn main()
+#![windows_subsystem = "windows"]
+
+use windows::{
+  core::*,
+  ApplicationModel::Activation::LaunchActivatedEventArgs,
+  Win32::System::Com::*,
+  UI::Xaml::Controls::TextBox,
+  UI::Xaml::{Application, ApplicationInitializationCallback, Window},
+};
+
+use windows as Windows;
+
+#[implement(
+  extend Windows::UI::Xaml::Application,
+  override OnLaunched
+)]
+struct MyApp();
+
+#[allow(non_snake_case)]
+impl MyApp
 {
-  println!("Project X, Initiated.");
+  fn OnLaunched(&self, _: &Option<LaunchActivatedEventArgs>) -> Result<()>
+  {
+    let window = Window::Current()?;
+    window.SetContent(TextBox::new()?)?;
+    window.Activate()
+  }
+}
+
+fn main() -> Result<()>
+{
+  unsafe {
+    CoInitializeEx(std::ptr::null_mut(), COINIT_MULTITHREADED)?;
+  }
+  Application::Start(ApplicationInitializationCallback::new(|_| {
+    MyApp().new()?;
+    Ok(())
+  }))
 }
